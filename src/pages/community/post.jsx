b82c25cb7b3aa5208cef6d7fe4d2bb924cf7a3a6
alszero <alszero05@gmail.com>
2023-06-13 20:56:12 +0900

    커뮤니티 페이지 수정

--- src/pages/community/post.jsx
+++ src/pages/community/post.jsx
@@ -2,22 +2,33 @@ import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import styled from 'styled-components';
+import Header from '../Header';
 
 const Container = styled.div`
-  max-width: 600px;
-  margin: 10% auto 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+`;
+
+const FormWrapper = styled.div`
+  width: 80vw;
+  max-width: 1000px;
   padding: 20px;
-  background-color: #ffffff;
+  background-color: #f8f8f8;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   border-radius: 5px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
 `;
 
 const Title = styled.h2`
   margin-top: 0;
+  font-size: 24px;
+  color: #333;
 `;
 
 const Form = styled.form`
@@ -31,13 +42,38 @@ const FormGroup = styled.div`
 const Label = styled.label`
   display: block;
   margin-bottom: 5px;
+  font-weight: 500;
 `;
 
 const Input = styled.input`
-  width: 96%;
+  width: calc(100% - 20px);
   padding: 10px;
   border: 1px solid #ccc;
   border-radius: 4px;
+  font-size: 16px;
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  height: 150px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  resize: vertical;
+`;
+
+const EditorContainer = styled.div`
+  .ql-container {
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+
+  .ql-editor {
+    min-height: 200px;
+    font-size: 16px;
+    padding: 10px;
+  }
 `;
 
 const SubmitButton = styled.button`
@@ -49,13 +85,18 @@ const SubmitButton = styled.button`
   border: none;
   border-radius: 4px;
   cursor: pointer;
+  font-size: 16px;
+  text-transform: uppercase;
 
   &:hover {
     background-color: #45a049;
   }
 `;
 
-function BulletinBoard() {
+function POST() {
+  const [isToggled, setIsToggled] = useState(false);
+  const [userToggled, setUserToggled] = useState(false);
+
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [tags, setTags] = useState('');
@@ -100,42 +141,54 @@ function BulletinBoard() {
   };
 
   return (
-    <Container>
-      <Title>글 작성하기</Title>
-      <Form onSubmit={handleSubmit}>
-        <FormGroup>
-          <Label htmlFor="title">제목</Label>
-          <Input
-            type="text"
-            id="title"
-            value={title}
-            onChange={handleTitleChange}
-            placeholder="제목을 입력하세요"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label htmlFor="content">내용</Label>
-          <ReactQuill
-            value={content}
-            onChange={handleContentChange}
-            placeholder="내용을 입력하세요"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label htmlFor="tags">해시태그</Label>
-          <Input
-            type="text"
-            id="tags"
-            value={tags}
-            onChange={handleTagsChange}
-            onKeyDown={handleKeyDown}
-            placeholder="해시태그를 입력하세요"
-          />
-        </FormGroup>
-        <SubmitButton type="submit">글 작성</SubmitButton>
-      </Form>
-    </Container>
+    <>
+      <Header
+        isToggled={isToggled}
+        userToggled={userToggled}
+        setIsToggled={setIsToggled}
+        setUserToggled={setUserToggled}
+      />
+      <Container>
+        <FormWrapper>
+          <Title>글 작성하기</Title>
+          <Form onSubmit={handleSubmit}>
+            <FormGroup>
+              <Label htmlFor="title">제목</Label>
+              <Input
+                type="text"
+                id="title"
+                value={title}
+                onChange={handleTitleChange}
+                placeholder="제목을 입력하세요"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="content">내용</Label>
+              <EditorContainer>
+                <ReactQuill
+                  value={content}
+                  onChange={handleContentChange}
+                  placeholder="내용을 입력하세요"
+                />
+              </EditorContainer>
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="tags">해시태그</Label>
+              <Input
+                type="text"
+                id="tags"
+                value={tags}
+                onChange={handleTagsChange}
+                onKeyDown={handleKeyDown}
+                placeholder="해시태그를 입력하세요"
+              />
+            </FormGroup>
+            <SubmitButton type="submit">글 작성</SubmitButton>
+          </Form>
+        </FormWrapper>
+      </Container>
+    </>
   );
 }
 
-export default BulletinBoard;
+export default POST;
