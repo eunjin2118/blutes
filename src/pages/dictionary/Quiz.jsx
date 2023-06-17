551a2476d2ab2ca9ff85b844fbcb96dc802eb3e5
alszero <alszero05@gmail.com>
2023-06-18 07:16:48 +0900

    Add: 퀴즈 데이터 연결해 화면에 띄우기 성공

--- src/pages/dictionary/Quiz.jsx
+++ src/pages/dictionary/Quiz.jsx
@@ -1,64 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
 import axios from 'axios';
-import Header from '../Header';
-
-const QuizContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 50px;
-`;
-
-const QuizWord = styled.h3`
-  margin-bottom: 10px;
-  color: #333;
-`;
-
-const QuizOptions = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 20px;
-`;
-
-const QuizOption = styled.button`
-  width: 100%;
-  height: 50px;
-  margin-bottom: 10px;
-  background-color: #071DA1;
-  border-radius: 8px;
-  color: white;
-  border: none;
-`;
-
-const NextButton = styled.button`
-  width: 100px;
-  height: 50px;
-  margin-top: 20px;
-  background-color: green;
-  border-radius: 8px;
-  color: white;
-  border: none;
-`;
 
 const WordQuizPage = () => {
-  const [quizs, setQuizs] = useState([]);
+  const [quizzes, setQuizzes] = useState([]);
   const [currentQuizIndex, setCurrentQuizIndex] = useState(0);
-  const [score, setScore] = useState(0);
-  const [showNextButton, setShowNextButton] = useState(false);
-  const [isToggled, setIsToggled] = useState(false);
-  const [userToggled, setUserToggled] = useState(false);
+  const [selectedMeaning, setSelectedMeaning] = useState(null);
+  const [showResults, setShowResults] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        await axios.get('quiz')
-        .then((response) =>{
-          console.log(response);
-          const data = response.data.slice(0, 10); // 최대 10문제까지 설정
-          console.log(data);
-          setQuizs(data); // 단어 데이터 배열에 설정
-        })
+        const response = await axios.get('/quiz');
+        const data = response.data.slice(0, 10);
+        const quizzesData = data.map((item) => ({
+          word: item.word,
+          meanings: [item.meaning1, item.meaning2, item.meaning3],
+          correctMeaning: item.correctMeaning,
+        }));
+        setQuizzes(quizzesData);
       } catch (error) {
         console.log(error);
       }
@@ -66,59 +25,80 @@ const WordQuizPage = () => {
     fetchData();
   }, []);
 
-  const handleOptionClick = (isCorrect) => {
-    if (isCorrect) {
-      setScore((prevScore) => prevScore + 1);
-    }
-    setShowNextButton(true);
-  };
-
-  const handleNextButtonClick = () => {
-    setShowNextButton(false);
-    if (currentQuizIndex < 9) { // 10문제까지만 퀴즈 진행
-      setCurrentQuizIndex((prevIndex) => prevIndex + 1);
+  const handleNextQuestion = () => {
+    if (currentQuizIndex === quizzes.length - 1) {
+      setShowResults(true);
     } else {
-      console.log('퀴즈 종료. 점수:', score);
+      setCurrentQuizIndex((prevIndex) => prevIndex + 1);
+      setSelectedMeaning(null);
     }
   };
 
-  const currentWord = quizs[currentQuizIndex];
+  const handleSelectMeaning = (meaning) => {
+    setSelectedMeaning(meaning);
+  };
+
+  const handleRestartQuiz = () => {
+    setCurrentQuizIndex(0);
+    setSelectedMeaning(null);
+    setShowResults(false);
+  };
+
+  if (quizzes.length === 0) {
+    return <div>Loading...</div>;
+  }
+
+  const currentQuiz = quizzes[currentQuizIndex];
+  const { word, meanings, correctMeaning } = currentQuiz;
 
   return (
-    <>
-      {/* Header 컴포넌트 */}
-      <Header
-        isToggled={isToggled}
-        userToggled={userToggled}
-        setIsToggled={setIsToggled}
-        setUserToggled={setUserToggled}
-      />
-
-      <QuizContainer>
-        {/* 단어 목록이 존재하는 경우 */}
-        {quizs.length > 0 && (
-          <>
-            {/* 현재 단어 표시 */}
-            <QuizWord>{currentWord.quizs}</QuizWord>
-            {/* 선택지 표시 */}
-            <QuizOptions>
-              {currentWord.options.map((option, index) => (
-                <QuizOption
-                  key={index}
-                  onClick={() => handleOptionClick(option.isCorrect)}
-                >
-                  {option.meaning}
-                </QuizOption>
+    <div>
+      <h2>Word Quiz</h2>
+      {!showResults ? (
+        <>
+          <p>Question {currentQuizIndex + 1}</p>
+          <p>Word: {word}</p>
+          <p>Select one meaning:</p>
+          <ul>
+            {meanings &&
+              meanings.map((meaning, index) => (
+                <li key={index}>
+                  <button
+                    className={selectedMeaning === meaning ? 'selected' : ''}
+                    onClick={() => handleSelectMeaning(meaning)}
+                    disabled={selectedMeaning !== null}
+                  >
+                    {meaning}
+                  </button>
+                </li>
               ))}
-            </QuizOptions>
-            {/* 다음 버튼 표시 */}
-            {showNextButton && (
-              <NextButton onClick={handleNextButtonClick}>다음</NextButton>
-            )}
-          </>
-        )}
-      </QuizContainer>
-    </>
+          </ul>
+          <div>
+            <p>Selected meaning:</p>
+            <p>{selectedMeaning}</p>
+            <button onClick={handleNextQuestion} disabled={selectedMeaning === null}>
+              {currentQuizIndex === quizzes.length - 1 ? 'Finish Quiz' : 'Next Question'}
+            </button>
+          </div>
+        </>
+      ) : (
+        <div>
+          <h2>Quiz Results</h2>
+          <p>You have completed the quiz!</p>
+          {quizzes.map((quiz, index) => (
+            <div key={index}>
+              <p>Question {index + 1}</p>
+              <p>Word: {quiz.word}</p>
+              <p>Correct Meaning: {quiz.meanings[quiz.correctMeaning]}</p>
+              <p>Your Selected Meaning:</p>
+              <p>{selectedMeaning}</p>
+              <hr />
+            </div>
+          ))}
+          <button onClick={handleRestartQuiz}>Restart Quiz</button>
+        </div>
+      )}
+    </div>
   );
 };
 
