af4d835bd1cf2b2e75f81092fb1b22a5d6c36a83
alszero <alszero05@gmail.com>
2023-06-17 12:47:50 +0900

    FIX: WordList 버튼 추가

--- src/pages/dictionary/WordList.jsx
+++ src/pages/dictionary/WordList.jsx
@@ -3,9 +3,36 @@ import styled from 'styled-components';
 import Header from "../Header.js";
 import axios from 'axios';
 
+const SearchContainer = styled.div`
+  position: relative;
+  width: 700px;
+  display: flex;
+  align-items: center;
+  margin: 50px auto;
+`;
+
+const SearchInput = styled.input`
+  width: 100%;
+  border: 1px solid #bbb;
+  border-radius: 8px;
+  padding: 10px 12px;
+  font-size: 14px;
+  border-radius: 20px;
+`;
+
+const SearchIcon = styled.img`
+  position: absolute;
+  width: 17px;
+  top: 50%;
+  right: 12px;
+  transform: translateY(-50%);
+  margin: 0;
+`;
+
 const Container = styled.div`
-  margin-top: 8%;
   display: grid;
+  margin-left : 2.5%;
+  margin-right: 2.5%;
   grid-template-columns: repeat(4, 1fr); /* 4개의 열로 구성 */
   grid-gap: 20px; /* 카드 간격 조정 */
 `;
@@ -15,7 +42,8 @@ const Card = styled.div`
   padding: 20px;
   background-color: #fff;
   border-radius: 8px;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
+  border: 1px solid #071DA1;
 `;
 
 const Word = styled.h3`
@@ -32,6 +60,41 @@ const Sentence = styled.p`
   color: #777;
 `;
 
+const TopContainer = styled.div `
+  display: flex; 
+  justify-content: space-between;
+`;
+
+const MyWords = styled.h2` 
+  margin-top: 50px;
+  margin-left: 3%
+`;
+
+const Buttons = styled.div `
+  margin-top: 50px;
+  display: flex;
+  margin-right: 3%
+`;
+
+const Addword = styled.button `
+  width: 100px;
+  height: 50px;
+  background-color: #071DA1;
+  border-radius: 8px;
+  color: white;
+  border: none;
+`;
+
+const StartQuiz = styled.button `
+  width: 100px;
+  height: 50px;
+  margin-left: 1%;
+  background-color: green;
+  border-radius: 8px;
+  color: white;
+  border: none;
+`;
+
 const WordList = () => {
   const [words, setWords] = useState([]); // 단어 데이터 배열
   const [isToggled, setIsToggled] = useState(false);
@@ -46,6 +109,11 @@ const WordList = () => {
     })
   }, []);
 
+  // AddWordForm페이지로 이동하는 버튼
+  // const handlePostButtonClick = () => {
+  //   navigate('/post'); // '/post' 경로로 이동
+  // };
+
   return (
     <>
       <Header
@@ -54,6 +122,19 @@ const WordList = () => {
       setIsToggled={setIsToggled}
       setUserToggled={setUserToggled}
       />
+      <SearchContainer>
+        <SearchInput type="text" placeholder="검색어 입력" />
+        <SearchIcon src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png" />
+      </SearchContainer>
+      <hr />
+      <TopContainer>
+        <MyWords>나의 단어장</MyWords>
+        <Buttons>
+          <Addword className='AddWord'>단어추가</Addword>
+          <StartQuiz className='StartQuiz'>퀴즈보기</StartQuiz>
+        </Buttons>
+      </TopContainer>
+      <br/>
       <Container>
         {words.map((word) => (
           <Card key={word.id}>
